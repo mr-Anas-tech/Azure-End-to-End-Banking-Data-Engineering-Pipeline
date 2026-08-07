9ee4500dd428d7bc0854de451c0a050f7e9471aa
Muhammad Anas <mranaskhudabukh@gmail.com>
2026-08-07 06:07:39 +0000

    Solve the duplication of Transaction_id by window function

--- models/BANKING/Marts/fct_banking_transactions.sql
+++ models/BANKING/Marts/fct_banking_transactions.sql
@@ -4,9 +4,11 @@ with transactions as (
 
 users as (
     select * from {{ ref('stg_banking_users') }}
-)
+),
 
-select
+joined_as(
+
+    select
     t.transaction_id,
     t.user_id,
 
@@ -19,8 +21,31 @@ select
     u.country as customer_country,
     u.city as customer_city,
     u.gender as customer_gender,
-    u.age as customer_age
+    u.age as customer_age,
+    Row_NUMBER() OVER(
+        partition by t.transaction_id
+        ORDER by t.transaction_timestamp DESC
+    ) AS rn
 
 from transactions t
 left join users u
     on t.user_id = u.user_id
+)
+
+
+select
+transaction_id,
+user_id,
+transaction_type,
+amount,
+transaction_timestamp,
+customer_name,
+customer_email,
+customer_country,
+customer_city,
+customer_gender,
+customer_age
+from joined_as
+where rn = 1
+
+
